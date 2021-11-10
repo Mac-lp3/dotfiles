49db32c797af33524d7a43bc1161988738fa7acd
Mac-lp3 <staticlp3@gmail.com>
2021-11-10 21:13:51 +1300

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+
+## Dependencies
+
+### terminal
+
+* Nerd font Sauce Code Pro
+* Nord theme
+
+### shell
+
+* oh-my-zsh
+
+### neovim
+
+* vim-plug
---
+++ sync.sh
@@ -0,0 +1,10 @@
+# This script copies the latest version of dotfiles I want tracked.
+# Ideally, no direct changes to this repo should be made - it should all go through this script.
+
+
+# TODO
+# rm rf oh-my-zsh/custom
+# cp .../custom
+# same with nvim
+# same with .zshrc
+# set up script (download dir colors, etc)
